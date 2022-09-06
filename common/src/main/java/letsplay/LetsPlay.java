a4dc0c3590e53069ab81e8b96e023ee1a356ddcf
Jonathan Ho <jonafanho@gmail.com>
2022-09-06 13:35:54 -0700

    Added Chunky commands

--- common/src/main/java/letsplay/LetsPlay.java
+++ common/src/main/java/letsplay/LetsPlay.java
@@ -1,10 +1,36 @@
 package letsplay;
 
+import letsplay.mappings.Utilities;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.MinecraftServer;
+
 public class LetsPlay {
 
 	public static final String MOD_ID = "letsplay";
 
 	public static void init() {
 
+	}
+
+	public static void onPlayerJoin(MinecraftServer server) {
+		final CommandSourceStack commandSourceStack = server.createCommandSourceStack();
+		runCommand(server, commandSourceStack, "/chunky pause");
+		System.out.println("Server player count: " + server.getPlayerCount());
+	}
+
+	public static void onPlayerQuit(MinecraftServer server) {
+		final int playerCount = server.getPlayerCount();
+		if (playerCount == 0) {
+			final CommandSourceStack commandSourceStack = server.createCommandSourceStack();
+			runCommand(server, commandSourceStack, "/chunky continue");
+			runCommand(server, commandSourceStack, "/chunky quiet 600");
+		} else {
+			System.out.println("Server player count: " + playerCount);
+		}
+	}
+
+	private static void runCommand(MinecraftServer server, CommandSourceStack commandSourceStack, String command) {
+		System.out.println("Running command " + command);
+		Utilities.sendCommand(server, commandSourceStack, command);
 	}
 }
--- fabric/src/main/java/letsplay/LetsPlayFabric.java
+++ fabric/src/main/java/letsplay/LetsPlayFabric.java
@@ -1,11 +1,14 @@
 package letsplay;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 public class LetsPlayFabric implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
 		LetsPlay.init();
+		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> LetsPlay.onPlayerJoin(server));
+		ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> LetsPlay.onPlayerQuit(server));
 	}
 }
